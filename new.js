b0a85b8755604f46bb223ba7fc8fc2736b45377c
Monalisa Patra <pmonalisa603@gmail.com>
2024-12-13 18:40:12 +0530

    Some Change

---
+++ new.js
@@ -0,0 +1,67 @@
+
+
+var swiper new Swiper(".mySwiper", {
+
+loop: true,
+
+autoplay: true,
+
+autoplayTimeout: 1000,
+
+autoplayHoverPause: true,
+
+effect: "coverflow",
+
+grabCursor: true,
+
+85
+
+centeredSlides: true,
+
+86,
+
+slidesPerView: "auto",
+
+87:
+
+coverflowEffect: (
+
+88,
+
+I rotate: е,
+
+89,
+
+stretch: 8,
+
+98,
+
+91,
+
+92,
+
+93
+
+94,
+
+95,
+
+depth : 300,
+
+modifier: 1,
+
+slideShadows: false,
+
+pagination: (
+
+el: ".swiper-pagination",
+
+96
+
+97
+
+});
+
+98
+
+</script>
